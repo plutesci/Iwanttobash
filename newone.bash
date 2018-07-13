885e527c302134c58dba1cd71ffad5b5abbac179
plutesci <shaundl@icloud.com>
2018-07-14 00:18:09 +0100

    a way to use the banner feature and a example it just prints newone in two examples

---
+++ newone.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Just a example of a echo from the two tools figlet, and sysvbanner
+# install sudo apt-get install sysvbanner figlet. This is for ubuntu seek
+# distro guidelines 
+clear
+figlet "newone 	enowen
+	NewOne	enOweN"
+banner "newone"
+
+
+
+figlet "newone"
